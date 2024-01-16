5048df59a827f3f7519e384bb57492a19aedcc59
M. J. Fromberger <michael.j.fromberger@gmail.com>
2024-01-15 17:02:51 -0800

    Fix rate limit handling.
    Separate read and write rates. Add checks where needed.

--- s3store.go
+++ s3store.go
@@ -24,20 +24,25 @@ import (
 //
 //	[prefix@]bucket:region
 func Opener(_ context.Context, addr string) (blob.Store, error) {
-	parts := strings.SplitN(addr, "@", 2)
-	prefix, bucketRegion := "", parts[0]
-	if len(parts) == 2 {
-		prefix, bucketRegion = parts[0], parts[1]
+	prefix, bucketRegion, ok := strings.Cut(addr, "@")
+	if !ok {
+		prefix, bucketRegion = bucketRegion, prefix
 	}
-	parts = strings.SplitN(bucketRegion, ":", 2)
-	if len(parts) != 2 {
+	bucket, region, ok := strings.Cut(bucketRegion, ":")
+	if !ok {
 		return nil, errors.New("invalid S3 address, requires bucket:region")
 	}
-	return New(parts[0], parts[1], &Options{
+
+	return New(bucket, region, &Options{
 		// Per https://docs.aws.amazon.com/AmazonS3/latest/userguide/optimizing-performance.html
-		// the rate limit for PUT/DELETE operations is 3500qps per prefix.
-		RateLimit: rate.NewLimiter(3500, 64),
-		KeyPrefix: prefix,
+		// the rate limit for PUT/DELETE operations is 3500qps per prefix and
+		// the rate limit for GET operations is 5500qps per prefix.
+		// This appears to include the "empty" prefix for writing the root of the bucket.
+		// Moreover, it can take a while for the service to "scale up" to the
+		// full load, so here we choose a lower bound.
+		ReadRateLimit:  rate.NewLimiter(1000, 1),
+		WriteRateLimit: rate.NewLimiter(500, 1),
+		KeyPrefix:      prefix,
 	})
 }
 
@@ -49,7 +54,8 @@ type Store struct {
 	session   *session.Session
 	svc       *s3.S3
 	keyPrefix string
-	limit     Waiter
+	rlimit    Waiter
+	wlimit    Waiter
 }
 
 // New creates a new Store that references the given bucket and region.
@@ -84,7 +90,8 @@ func New(bucket, region string, opts *Options) (*Store, error) {
 		session:   sess,
 		svc:       svc,
 		keyPrefix: opts.keyPrefix(),
-		limit:     opts.rateLimit(),
+		rlimit:    opts.readRateLimit(),
+		wlimit:    opts.writeRateLimit(),
 	}, nil
 }
 
@@ -92,7 +99,10 @@ func New(bucket, region string, opts *Options) (*Store, error) {
 func (s *Store) Get(ctx context.Context, key string) ([]byte, error) {
 	if key == "" {
 		return nil, blob.KeyNotFound(key)
+	} else if !s.waitRead(ctx) {
+		return nil, fmt.Errorf("rate limit: %w", ctx.Err())
 	}
+
 	obj, err := s.svc.GetObject(&s3.GetObjectInput{
 		Bucket: &s.bucket,
 		Key:    aws.String(s.encodeKey(key)),
@@ -108,15 +118,16 @@ func (s *Store) Get(ctx context.Context, key string) ([]byte, error) {
 
 // Put writes a blob to the store.
 func (s *Store) Put(ctx context.Context, opts blob.PutOptions) error {
-	if !s.waitLimit(ctx) {
-		return fmt.Errorf("rate limit: %w", ctx.Err())
-	}
 	if opts.Key == "" {
 		return blob.KeyNotFound(opts.Key)
 	} else if !opts.Replace {
 		if s.keyExists(ctx, opts.Key) == nil {
 			return blob.KeyExists(opts.Key)
 		}
+	}
+
+	if !s.waitWrite(ctx) {
+		return fmt.Errorf("rate limit: %w", ctx.Err())
 	}
 	awsKey := aws.String(s.encodeKey(opts.Key))
 	if _, err := s.svc.PutObject(&s3.PutObjectInput{
@@ -131,11 +142,11 @@ func (s *Store) Put(ctx context.Context, opts blob.PutOptions) error {
 
 // Delete atomically removes a blob from the store.
 func (s *Store) Delete(ctx context.Context, key string) error {
-	if !s.waitLimit(ctx) {
-		return fmt.Errorf("rate limit: %w", ctx.Err())
-	}
 	if err := s.keyExists(ctx, key); err != nil {
 		return err
+	}
+	if !s.waitWrite(ctx) {
+		return fmt.Errorf("rate limit: %w", ctx.Err())
 	}
 	_, err := s.svc.DeleteObject(&s3.DeleteObjectInput{
 		Bucket: &s.bucket,
@@ -148,7 +159,10 @@ func (s *Store) Delete(ctx context.Context, key string) error {
 func (s *Store) keyExists(ctx context.Context, key string) error {
 	if key == "" {
 		return blob.KeyNotFound(key) // S3 does not accept empty keys
+	} else if !s.waitRead(ctx) {
+		return fmt.Errorf("rate limit: %w", ctx.Err())
 	}
+
 	obj, err := s.svc.HeadObject(&s3.HeadObjectInput{
 		Bucket: &s.bucket,
 		Key:    aws.String(s.encodeKey(key)),
@@ -176,6 +190,9 @@ func (s *Store) List(ctx context.Context, start string, f func(string) error) er
 		// it in the sequence.
 	}
 	for {
+		if !s.waitRead(ctx) {
+			return fmt.Errorf("rate limit: %w", ctx.Err())
+		}
 		pg, err := s.svc.ListObjectsV2(req)
 		if err != nil {
 			return err
@@ -210,6 +227,7 @@ func (s *Store) Len(ctx context.Context) (int64, error) {
 	defer cancel()
 	g := taskgroup.New(taskgroup.Trigger(cancel))
 
+	// Note we don't need to check rate limits here, because List already does.
 	var total int64
 	c := taskgroup.NewCollector(func(v int64) { total += v })
 	for i := 0; i < 256; i++ {
@@ -243,7 +261,8 @@ type Options struct {
 	KeyPrefix string
 
 	// If set, us this rate limiter for requests to the store.
-	RateLimit Waiter
+	ReadRateLimit  Waiter
+	WriteRateLimit Waiter
 
 	// An optional AWS configuration to use when constructing the session.
 	AWSConfig *aws.Config
@@ -274,11 +293,18 @@ func (o *Options) keyPrefix() string {
 	return o.KeyPrefix
 }
 
-func (o *Options) rateLimit() Waiter {
+func (o *Options) readRateLimit() Waiter {
 	if o == nil {
 		return nil
 	}
-	return o.RateLimit
+	return o.ReadRateLimit
+}
+
+func (o *Options) writeRateLimit() Waiter {
+	if o == nil {
+		return nil
+	}
+	return o.WriteRateLimit
 }
 
 // prevKey returns the string that is immediately lexicographically prior to
@@ -302,11 +328,18 @@ func (s *Store) encodeKey(key string) string {
 	return s.keyPrefix + hex.EncodeToString([]byte(key))
 }
 
-func (s *Store) waitLimit(ctx context.Context) bool {
-	if s.limit == nil {
+func (s *Store) waitWrite(ctx context.Context) bool {
+	if s.wlimit == nil {
 		return true
 	}
-	return s.limit.Wait(ctx) == nil
+	return s.wlimit.Wait(ctx) == nil
+}
+
+func (s *Store) waitRead(ctx context.Context) bool {
+	if s.rlimit == nil {
+		return true
+	}
+	return s.rlimit.Wait(ctx) == nil
 }
 
 var errNotMyKey = errors.New("not a blob key")
